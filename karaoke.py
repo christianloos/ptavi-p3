d55b54aea44173c0248788147299e28cd5df035c
Christian <christianloos43@gmail.com>
2016-10-10 22:36:11 +0200

    Pasado por PEP8

--- karaoke.py
+++ karaoke.py
@@ -9,55 +9,50 @@ import json
 from urllib.request import urlretrieve
 
 try:
-    first_json = sys.argv[1]
+    smil_file = sys.argv[1]
 except IndexError:
-    sys.exit('Usage: python3 karaoke.py file.smil.')
+    sys.exit('Usage: python3 karaoke.py file.smil')
+
 
 class KaraokeLocal():
 
-    def __init__(self, smil_file):
-        self.data = ""
+    def __init__(self):
         parser = make_parser()
-        self.handler = SmallSMILHandler()
-        parser.setContentHandler(self.handler)
-        parser.parse(open(smil_file))
-        self.atr_list = self.handler.get_tags()
+        handler = SmallSMILHandler()
+        parser.setContentHandler(handler)
+        parser.parse(open(sys.argv[1]))
+        self.tags = handler.get_tags()
 
-    def __str__(self):
-        data = ""
-        for tag in self.atr_list:
-            name = tag[0]
-            data += name
-            atributes = tag[1]
-            for atribute in atributes:
-                if atributes[atribute] != "":
-                    data += "\t" + atribute + '="' + atributes[atribute] + '"'
-            data += "\n"
-        return data
-           
+    def __srt__(self):
+        data = ''
+        for line in self.tags:
+            data = data + line['tag'] + '\t'
+            for atribute in line:
+                if atribute != 'tag' and line[atribute] != "":
+                    data = data + atribute + ' = "' + line[atribute] + '"\t'
+            data = data + '\n'
+        print(data)
 
-    def to_json(self, smil_file, json_file = first_json):
-        first_json = sys.argv[1][:-4]+"json"
-        json_file = open(json_file, 'w')
-        json.dump(self.atr_list, json_file)
+    def to_json(self, smil_file, json_file):
+        json = sys.argv[1][:-4] + 'json'
+        json_file = open(json, 'w')
+        json.dump(self.tags, json_file)
 
     def do_local(self):
-        for tag in self.tags:
-            atts = tag[1]
-            for att in atts:
-                if atts[att][0:7] == "http://":
-                    os.system("wget -q " + atts[att])
-                    campos = atts[att].split('/')
-                    atts[att] = campos[-1]    
-        
-    
+        for line in self.tags:
+            for tag in line:
+                for atribute in line['tag']:
+                    if line['tag'][atribute][:7] == "http://":
+                        urlretrieve(line[tag][atribute])
+                        url = line[tag][atribute].split('/')
+                        line[tag][atribute] = url[-1]
 
 if __name__ == "__main__":
 
     smil_file = sys.argv[1]
-    karaoke = KaraokeLocal(smil_file)
-    karaoke.__str__()
+    karaoke = KaraokeLocal()
+    karaoke.__srt__()
     karaoke.to_json(smil_file)
     karaoke.do_local()
     karaoke.to_json(smil_file, "local.json")
-    karaoke.__str__()          
+    karaoke.__srt__()
