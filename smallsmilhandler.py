e90783016d5893ae8541f325b546934546a6436c
Christian <christianloos43@gmail.com>
2016-10-10 21:18:22 +0200

    Finalizado

--- smallsmilhandler.py
+++ smallsmilhandler.py
@@ -4,34 +4,33 @@
 from xml.sax import make_parser
 from xml.sax.handler import ContentHandler
 
+
 class SmallSMILHandler(ContentHandler):
 
     def __init__(self):
-        self.atr_list = []
-        self.atr_root_layout = ['width', 'height', 'background_color']
-        self.atr_region = ['id', 'top', 'bottom', 'left', 'right']
-        self.atr_img = ['src', 'region', 'begin', 'dur']
-        self.atr_audio = ['src', 'begin', 'dur']
-        self.atr_textstream = ['src', 'region']
+        self.tag_dicc = {
+            'root-layout': ['width', 'height',
+                            'background-color'],
+            'region': ['id', 'top', 'bottom', 'left', 
+                       'right'],
+            'img': ['src', 'region', 'begin', 'dur'],
+            'audio': ['src', 'begin', 'dur'],
+            'textstream': ['src', 'region']}
+        self.tags = []
 
-        self.tags = {'root_layout': self.atr_root_layout,
-                     'region': self.atr_region,
-                     'img': self.atr_img,
-                     'audio': self.atr_audio,
-                     'textstream': self.atr_textstream}
+    def startElement(self, name, attrs):
+        if name in self.tag_dicc:
+            atributes = {}
+            atributes['tag'] = name
+            for atribute in self.tag_dicc[name]:
+                atributes[atribute] = attrs.get(atribute, "")
+            self.tags.append(atributes)
 
-    def startElement(self, tag, attrs):
-        self.smil_data = {}
-        self.atr_list.append(tag)
-        if tag in self.tags:            
-            for atribute in self.tags[tag]:
-                self.smil_data[atribute] = attrs.get(atribute, "")
-            self.atr_list.append([tag, self.smil_data])
-            
     def get_tags(self):
-        return self.atr_list
-        
+        return self.tags
+
 if __name__ == "__main__":
+
     parser = make_parser()
     handler = SmallSMILHandler()
     parser.setContentHandler(handler)
